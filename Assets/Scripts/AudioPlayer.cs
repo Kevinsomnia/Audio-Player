059e40ea4d54a6fd2da2581e6e0a701d3a328a66
Hanan Au <hanhan1280@gmail.com>
2022-10-04 13:39:45 -0400

    Stream Audio??

--- Assets/Scripts/AudioPlayer.cs
+++ Assets/Scripts/AudioPlayer.cs
@@ -1,4 +1,6 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class AudioPlayer : MonoBehaviour
 {
@@ -37,17 +39,41 @@ public class AudioPlayer : MonoBehaviour
 
         if (!string.IsNullOrEmpty(_initialURL))
         {
-            PlayURL(_initialURL);
+            // PlayURL(_initialURL);
         }
+
     }
 
-    public void PlayURL(string url)
+    public void PlayURL(string url, AudioType audioType)
     {
-        _audioSource.Play();
+        StartCoroutine(Start(url, audioType));
     }
 
     private void OnAudioRead(float[] data)
     {
 
+    }
+
+    private IEnumerator Start(string url, AudioType audioType)
+    {
+        using (var webRequest = UnityWebRequestMultimedia.GetAudioClip(url, audioType))
+        {
+            ((DownloadHandlerAudioClip)webRequest.downloadHandler).streamAudio = true;
+
+            webRequest.SendWebRequest();
+            while (webRequest.result != UnityWebRequest.Result.ConnectionError && webRequest.downloadedBytes < 1024)
+                // Ensure that we have at least 1 byte to stream
+                yield return null;
+
+            if (webRequest.result == UnityWebRequest.Result.ConnectionError)
+            {
+                Debug.LogError(webRequest.error);
+                yield break;
+            }
+
+            var clip = ((DownloadHandlerAudioClip)webRequest.downloadHandler).audioClip;
+            _audioSource.clip = clip;
+            _audioSource.Play();
+        }
     }
 }
